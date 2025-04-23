0283f2df871204c8bb2f382975d96c3d12d5ea62
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-04-23 15:22:17 +0200

    FIX: Update import path for run_app in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Put all newly developped code and log in src2 folder.
 We will merge the code to src folder after development is done.
 """
 
-from src_reform_app.src.app import run_app
+from src.app import run_app
 
 
 
