41c5f5a9565cf2c546cbebe03ca82bdf4e68a5d1
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-04-23 15:20:56 +0200

    FEAT: Add main application entry point and initial documentation

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This notes are git feature/reform-app.
+
+Put all newly developped code and log in src2 folder.
+We will merge the code to src folder after development is done.
+"""
+
+from src_reform_app.src.app import run_app
+
+
+
+
+
+
+if __name__ == "__main__":
+    run_app()
