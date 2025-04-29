22a303f7dfd8cbda24e191ef9361d7d62d55856e
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-04-29 13:52:38 +0200

    REF: Refactor main menu controller to improve structure and add message handling functions

--- src/controllers/main_menu_controller.py
+++ src/controllers/main_menu_controller.py
@@ -3,6 +3,10 @@
 
 
 # Call controllers
+
+from src.controllers.main_menu_controller import show_welcome, show_goodbye, ask_a_menu_choice
+
+
 from src.controllers.assay_lookup_controller import assay_lookup_flow
 
 from src.controllers.assay_logging_controller import log_experiment_flow, lookup_experiments_flow
@@ -11,72 +15,6 @@ from src.controllers.qc_controller import (add_qc_flow, lookup_qc_flow)
 
 
 
-def show_welcome():
-    print("Welcome to QuantiApp - Assay Management System!")
-
-def show_goodbye():
-    print("Goodbye!")
-
-
-
-
-
-
-def ask_menu_choice():
-    return input("Enter your choice: ").strip()
-                
-def ask_a_choice(question:str, choices:list):
-    """
-    Ask the user a question and return their choice.
-    """
-    print(question)
-    for i, choice in enumerate(choices, start=1):
-        print(f"{i}. {choice}")
-    
-    while True:
-        try:
-            user_choice = int(input("Enter your choice: "))
-            if 1 <= user_choice <= len(choices):
-                return choices[user_choice - 1]
-            else:
-                print("Invalid choice. Please try again.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-
-def ask_yes_no(question:str):
-    """
-    Ask the user a yes/no question and return their answer.
-    """
-    while True:
-        answer = input(f"{question} (y/n): ").strip().lower()
-        if answer in ['y', 'yes']:
-            return True
-        elif answer in ['n', 'no']:
-            return False
-        else:
-            print("Invalid input. Please enter 'y' or 'n'.")
-
-def ask_for_string(question:str):
-    """
-    Ask the user for a string input.
-    """
-    while True:
-        answer = input(f"{question}: ").strip()
-        if answer:
-            return answer
-        else:
-            print("Invalid input. Please enter a non-empty string.")
-
-def ask_for_number(question:str):
-    """
-    Ask the user for a number input.
-    """
-    while True:
-        try:
-            answer = int(input(f"{question}: ").strip())
-            return answer
-        except ValueError:
-            print("Invalid input. Please enter a number.")
 
 
 
@@ -104,7 +42,7 @@ def main_menu_flow(db_path):
 
     while True:
         show_main_menu()
-        choice = ask_menu_choice()
+        choice = ask_a_menu_choice()
 
         if choice == "1":
             assay_lookup_flow(db_path)
---
+++ src/controllers/messages_and_ask_questions.py
@@ -0,0 +1,72 @@
+"""
+There are functions to set up the welcome and goodbye messages, and to ask the user for input.
+"""
+
+
+
+def show_welcome():
+    print("Welcome to QuantiApp - Assay Management System!")
+
+def show_goodbye():
+    print("Goodbye!")
+
+
+
+
+
+
+def ask_a_menu_choice():
+    return input("Enter your choice: ").strip()
+                
+def ask_a_choice(question:str, choices:list):
+    """
+    Ask the user a question and return their choice.
+    """
+    print(question)
+    for i, choice in enumerate(choices, start=1):
+        print(f"{i}. {choice}")
+    
+    while True:
+        try:
+            user_choice = int(input("Enter your choice: "))
+            if 1 <= user_choice <= len(choices):
+                return choices[user_choice - 1]
+            else:
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def ask_yes_no(question:str):
+    """
+    Ask the user a yes/no question and return their answer.
+    """
+    while True:
+        answer = input(f"{question} (y/n): ").strip().lower()
+        if answer in ['y', 'yes']:
+            return True
+        elif answer in ['n', 'no']:
+            return False
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+def ask_for_string(question:str):
+    """
+    Ask the user for a string input.
+    """
+    while True:
+        answer = input(f"{question}: ").strip()
+        if answer:
+            return answer
+        else:
+            print("Invalid input. Please enter a non-empty string.")
+
+def ask_for_number(question:str):
+    """
+    Ask the user for a number input.
+    """
+    while True:
+        try:
+            answer = int(input(f"{question}: ").strip())
+            return answer
+        except ValueError:
+            print("Invalid input. Please enter a number.")
