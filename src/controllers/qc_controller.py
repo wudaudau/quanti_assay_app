833f000476b438f9d379eb5b7901551943cbc75f
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-04-29 14:09:28 +0200

    FEAT: Integrate user choice for QC type in add_qc_flow function

--- src/controllers/qc_controller.py
+++ src/controllers/qc_controller.py
@@ -1,6 +1,5 @@
 
-
-
+from src.controllers.messages_and_ask_questions import ask_a_choice
 
 def add_qc_flow(db_path):
     """
@@ -8,6 +7,37 @@ def add_qc_flow(db_path):
     """
     print("Adding a QC...")
     # Implement the logic to add a QC here
+
+    qc_type = ask_a_choice("Select QC type", ["Purchased", "Home made"])
+
+    if qc_type == "Purchased":
+        # Manufacture
+        # QC Cat Nº
+        # QC Lot Nº (UNIQUE)
+        # QC name
+        # Analyte name
+        # concentration
+        # Expiration date
+        # unit
+
+        # TODO: Link to assay? 
+            # Probably in another many-to-many table to link QC to assay
+            # Because QC could be used in different assays (e.g. MSD U-PLEX assays)
+        pass
+    elif qc_type == "Home made":
+        # QC Lot Nº (UNIQUE) # TODO: Need a convention for this
+        # QC name
+        # Analyte name
+        # concentration
+        # Preparation date
+        pass
+
+    # Ask the lot Nº
+    lot_number = input("Enter the lot number: ").strip()
+
+    # Check if the lot Nº is existing
+    # Implement the logic to check if the lot number exists in the database
+
     # For example, you can call the add_qc function from the qc_controller module
     # and pass the necessary parameters.
     # Example:
