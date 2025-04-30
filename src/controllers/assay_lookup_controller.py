7f4ba16bd86d4885e6039c80ae54628b653d7e96
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-04-30 10:49:23 +0200

    REF: Move assay_menu() and assay_menu_flow() to assay_menu_controller.py from assay_lookup_controller.py

--- src/controllers/assay_lookup_controller.py
+++ src/controllers/assay_lookup_controller.py
@@ -25,37 +25,6 @@ from src.assay_lookup.assay_lookup import (
 
 
 
-def assay_lookup_menu():
-    """
-    Display the assay lookup menu.
-    """
-    print("\nAssay Lookup Menu")
-    print("1. Lookup Assay (Full List)")
-    print("2. Lookup Assay (Step-by-Step Filter)")
-    print("3. Lookup Assay (Filter by Species and Analyte)")
-    print("4. Back to Main Menu")
-
-def assay_lookup_flow(db_path):
-    """
-    Main menu loop.
-    """
-    while True:
-        assay_lookup_menu()
-        choice = input("Enter your choice: ").strip()
-
-        if choice == "1":
-            lookup_assay_details_full(db_path)
-        elif choice == "2":
-            lookup_assay_details_step_by_step(db_path)
-        elif choice == "3":
-            lookup_assay_details_by_species_and_analyte(db_path)
-        elif choice == "4":
-            print("Returning to Main Menu...")
-            break
-
-
-
-
 
 
 def lookup_assay_details_full(db_path):
---
+++ src/controllers/assay_menu_controller.py
@@ -0,0 +1,47 @@
+"""
+This is the assay menu controller module.
+
+There are assay_menu and assay_menu_flow functions to display the assay menu and handle user input.
+"""
+
+
+from src.controllers.messages_and_ask_questions import show_menu_title
+
+from src.controllers.assay_lookup_controller import (
+    lookup_assay_details_full,
+    lookup_assay_details_step_by_step,
+    lookup_assay_details_by_species_and_analyte
+)
+
+
+def assay_menu():
+    """
+    Display the assay lookup menu.
+    """
+    print("\nAssay Menu")
+    # TODO: Add "new assay" option
+    print("1. Lookup Assay (Full List)")
+    print("2. Lookup Assay (Step-by-Step Filter)")
+    print("3. Lookup Assay (Filter by Species and Analyte)")
+    print()
+    print("4. Back to Main Menu")
+
+def assay_menu_flow(db_path):
+    """
+    Main menu loop.
+    """
+    while True:
+        show_menu_title("Assay Lookup Menu")
+        assay_menu()
+        choice = input("Enter your choice: ").strip()
+
+        if choice == "1":
+            lookup_assay_details_full(db_path) # TODO: Rename it to flow or controller
+        elif choice == "2":
+            lookup_assay_details_step_by_step(db_path) # TODO: Rename it to flow or controller
+        elif choice == "3":
+            lookup_assay_details_by_species_and_analyte(db_path) # TODO: Rename it to flow or controller
+        elif choice == "4":
+            print("Returning to Main Menu...")
+            break
+
