f55b3442fa0d748048fe39391042596ebc1f87a3
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-05-02 01:46:15 +0200

    REF: Refactor get_assay_details_by_name to use assay_lookup_view for improved query efficiency

--- src/assay_lookup/assay_lookup.py
+++ src/assay_lookup/assay_lookup.py
@@ -14,14 +14,9 @@ def get_assay_details_by_name(db_path, assay_name):
     cursor = conn.cursor()
 
     cursor.execute("""
-        SELECT a.name, s.name AS species, t.name AS assay_type, m.name AS manufacture, k.cat_number, k.format
-        FROM assay a
-        JOIN species s ON a.species_id = s.id
-        JOIN assay_type t ON a.assay_type_id = t.id
-        LEFT JOIN assays_kits ak ON a.id = ak.assay_id
-        LEFT JOIN kit k ON ak.kit_id = k.id
-        LEFT JOIN manufacture m ON k.manufacture_id = m.id
-        WHERE a.name = ?
+        SELECT assay_name, species, assay_type, manufacture, kit_cat_number, format 
+        FROM assay_lookup_view
+        WHERE assay_name = ?
     """, (assay_name,))
 
     results = cursor.fetchall()
