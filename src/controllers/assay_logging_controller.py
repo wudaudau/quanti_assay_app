21e1134aa65536466d3a65eeeb9d46033ec192e5
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-05-03 02:57:11 +0200

    REF: Refactor log_experiment_flow() to improve clarity and structure, adding TODOs for future enhancements

--- src/controllers/assay_logging_controller.py
+++ src/controllers/assay_logging_controller.py
@@ -64,56 +64,69 @@ def ask_experiment_date():
 
 def log_experiment_flow(db_path):
     """
+    (This function is similar to assay_lookup_flow_filter_by_species_and_assay_type() in assay_lookup_controller.py) # TODO: Refactor to avoid duplication using modules?
     """
 
-    print("\n--- Starting Experiment Logging Process ---")
-    print("Please follow the prompts to log your experiment.\n")
+    while True:
 
-    # Placeholder for the upcoming steps (species selection, assay selection, etc.)
-    # For now, just a simple message to confirm.
-    print("This is where the experiment logging process will continue...")
+        # TODO: Use show_flow_title_and_descriptions() to show the title and description of the flow
+        print("\n--- Starting Experiment Logging Process ---")
+        print("Please follow the prompts to log your experiment.\n")
 
-
-    # Select species
-    species = select_species(db_path)
-    if not species:
-        return
-
-    species_id, species_name = species
+        # Placeholder for the upcoming steps (species selection, assay selection, etc.)
+        # For now, just a simple message to confirm.
+        print("This is where the experiment logging process will continue...")
 
 
-    # Select assay type
-    assay_type = select_assay_type_by_species(db_path, species_id)
-    if not assay_type:
-        return
-
-    assay_type_id, assay_type_name = assay_type
-
-
-    # Select assay
-    assay = select_assay_by_species_and_type(db_path, species_id, assay_type_id)
-    if not assay:
-        return
-
-    assay_id, assay_name = assay
+        # 1) Ask species:
+            # Obtain species list from the database
+            # Use ask_a_choice() to show the list and get the user's choice
+        ls_species = fetch_ls_species_from_db(db_path)
+        if len(ls_species) == 0: # TODO: We need to test this case
+            print("No species found in the database.")
+            print("Back to the Assay Menu...")
+            return "assay menu"
+        else:
+            species_name = ask_a_choice("\nAvailable Species:", ls_species)
 
 
-    # Select sample type
-    sample_type = select_sample_type(db_path)
-    if not sample_type:
-        return
+        # 2) Ask assay type:
+        ls_assay_type = fetch_ls_assay_types_from_db_based_on_species(db_path, species_name)
+        if len(ls_assay_type) == 0: # TODO: We need to test this case
+            print("No assay types found for this species.")
+            print("Back to the Assay Menu...")
+            return "assay menu"
+        else:
+            assay_type = ask_a_choice("\nAvailable Assay Types:", ls_assay_type)
 
-    sample_type_id, sample_type_name = sample_type
 
-    # Select manipulators
-    manipulator_ids = ask_manipulator(db_path)
+        # 3) Ask assay name:
+        ls_assays = fetch_ls_assays_from_db_based_on_species_and_assay_type(db_path, species_name, assay_type)
+        if len(ls_assays) == 0: # TODO: We need to test this case
+            print("No assays found for this species and assay type.")
+            print("Back to the Assay Menu...")
+            return "assay menu"
+        else:
+            assay_name = ask_a_choice("\nAvailable Assays:", ls_assays)
 
 
-    exp_date = ask_experiment_date()
-        
+        # Select sample type
+        sample_type = select_sample_type(db_path)
+        if not sample_type:
+            return
+
+        sample_type_id, sample_type_name = sample_type
+
+        # Select manipulators
+        manipulator_ids = ask_manipulator(db_path)
+
+
+        exp_date = ask_experiment_date()
             
-
-    log_experiment(db_path, species_id, assay_type_id, assay_id, sample_type_id, manipulator_ids, exp_date)
+                
+        # TODO: Refactor this
+        # log_experiment(db_path, species_id, assay_type_id, assay_id, sample_type_id, manipulator_ids, exp_date)
+        print("(We need to refactor log_experiment() to log the data more efficiently.)")
 
 def lookup_experiments_flow(db_path, limit=None):
     """
