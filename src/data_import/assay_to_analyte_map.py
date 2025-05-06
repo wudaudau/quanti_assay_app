fe3b78b40e597f6d9b5ddcbaebb584f806fa98a2
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-05-07 00:53:52 +0200

    FEAT: Add ELISA assay to analyte mapping for database integration

---
+++ src/data_import/assay_to_analyte_map.py
@@ -0,0 +1,16 @@
+"""
+This will be integrated into the relatonal database. 
+"""
+
+# ELISA Assays only here
+ASSAY_TO_ANALYTES = {
+    "ELISA ApoE": "ApoE",
+    "ELISA B2M": "B2M",
+    "ELISA BAFF": "BAFF",
+    "ELISA CD14": "CD14",
+    "ELISA HLA-E": "HLA-E",
+    "ELISA IFABP": "IFABP",
+    "ELISA LPS": "LPS",
+    "ELISA S100B": "S100B",
+    "ELISA Zonulin": "Zonulin",
+}
